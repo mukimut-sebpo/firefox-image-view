8ae854fe31b3458afccac78d40c76536ca92684a
mukimut-sebpo <mukimut.salat@sebpo.com>
2024-07-30 23:01:04 +0600

    all files added

---
+++ script.js
@@ -0,0 +1,8 @@
+const img = document.querySelector('img');
+const video = document.querySelector('video');
+
+if(img) {
+    img.style.margin = 'unset'; 
+} else  if (video) {
+    video.style.margin = 'unset'; 
+}
